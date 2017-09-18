26adc2119449748edb4c36cd2d7087c4fa93cfba
BertZZ <bertie.wooles@hotmail.co.uk>
2017-09-18 01:04:55 +0100

    Changed email address in test

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User Can Sign Up" do
     click_link("here")
     fill_in 'user_username', with: 'BertZ'
     fill_in 'user_password', with: 'test1'
-    fill_in 'user_email', with: 'bertie.wooles@hotmail.co.uk'
+    fill_in 'user_email', with: 'test1@test.com'
     click_button('submit')
     expect(page).to have_content('User was successfully created. Thank you for signing up')
   end
